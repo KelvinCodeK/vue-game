ecbf53161ae6afcb32476ba6728001d604f5c48d
KelvinCodeK <kelvinkoeleman@hotmail.com>
2021-12-22 17:27:31 +0100

    basis gemaakt

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!-- op de character props kan je een index toevoegen die uit een data property komt wann characters worden gekozen / toegewezen -->
+    <your-character @first-attack="attackHandler" 
+    :character="character" 
+    :currentHealth="yourHealthBar"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import yourCharacter from './components/yourCharacter.vue'
+// Experience atc toevoegen met een localstorage
+// critical hit calculator
+
+const attackCalculator = (min, max) => {
+return Math.floor(Math.random() * (max - min  + 1) + min);
+}
 
 export default {
   name: 'App',
+  data() {
+    return {
+      yourHealthBar: 100,
+      opponentHealthBar: 100,
+      character: {
+        name: 'charizard',
+        attackOne: 'swipe',
+        attackTwo: 'lash',
+        attackThree: 'flame dance',
+        attackFour: 'boulder',
+      }
+    }
+  },
   components: {
-    HelloWorld
+    'your-character': yourCharacter
+  },
+  methods: {
+    attackHandler(min, max) {
+      const value = attackCalculator(min, max);
+      if(this.yourHealthBar - value <= 0) {
+        this.yourHealthBar = 0;
+      }
+      else {
+        this.yourHealthBar -= value;
+      }
+    }
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0px;
+  padding: 0px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/yourCharacter.vue
@@ -0,0 +1,79 @@
+<template>
+    <section>
+        <h2><em>{{ character.name }}</em></h2>
+        <div class="healthbar-wrapper">
+            <div 
+            class="health" 
+            :style="{width: currentHealth + '%'}" 
+            :class="[{'low-health': lowHealth}, {'medium-health': mediumHealth}]">
+            </div>
+        </div>
+        <button @click="firstAttack">{{ character.attackOne }}</button>
+        <button>{{ character.attackTwo }}</button>
+        <button>{{ character.attackThree }}</button>
+        <button>{{ character.attackFour }}</button>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'yourCharacter',
+  props: ['character', 'currentHealth'],
+  data() {
+      return {
+          mediumHealth: false,
+          lowHealth: false
+      }
+  },
+  watch: {
+      currentHealth(value) {
+          if( value > 20 && value <= 50 ) {
+              this.mediumHealth = true;
+          }
+          else if(value <= 20) {
+              this.lowHealth = true;
+          }
+      }
+  },
+  methods: {
+      firstAttack() {
+          this.$emit('first-attack', 5, 10);
+      }
+  }
+}
+</script>
+
+<style scoped>
+.health {
+    background-color: green;
+    height: 100%;
+    width: 100%;
+}
+
+.medium-health {
+    background-color: orange;
+}
+/* .low-health moet onder .health komen te staan. anders wordt de class niet doorgevoerd */
+.low-health {
+    background-color: red;
+}
+.healthbar-wrapper {
+    width: 20rem;
+    border: 1px solid black;
+    height: 2rem;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
